0a3df925a70fe2fcca068e1b315941b8af1002aa
WannouII <mathysprudhommealbert@gmail.com>
2024-07-15 17:06:51 +0200

    Player Update
    Le Joueur est maintenant visible et peut se déplacer !!

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from src.map import Map
 from src.player import Player
 
 
+
 class Main_game:
     def __init__(self):
         pg.init()
         self.screen = pg.display.set_mode((800, 600))
         pg.display.set_caption("Hello World")
+        self.clock = pg.time.Clock()
         self.running = True
 
         self.player = Player()
@@ -22,9 +24,19 @@ class Main_game:
         for event in pg.event.get():  # permet de recuperer les evenements (genre appuit des touches ect)
             if event.type == pg.QUIT:
                 self.running = False
-
+        keys = pg.key.get_pressed()
+        # Adjust player position based on key presses
+        if keys[pg.K_LEFT]:
+            self.player.move(-1, 0)
+        if keys[pg.K_RIGHT]:
+            self.player.move(1, 0)
+        if keys[pg.K_UP]:
+            self.player.move(0, -1)
+        if keys[pg.K_DOWN]:
+            self.player.move(0, 1)
     def draw(self):
         self.screen.fill((0, 0, 0))
+        self.player.Draw(self.screen)
         pg.display.flip()
 
     def run(self):
@@ -32,6 +44,7 @@ class Main_game:
         while self.running:
             self.update()
             self.draw()
+            self.clock.tick(60)
 
         pg.quit()
 
--- src/player.py
+++ src/player.py
@@ -3,15 +3,22 @@ import pygame
 
 class Player:
     def __init__(self):
-        self.pos = [0, 0]
+        self.pos = [50, 50]
         self.rect = pygame.rect.Rect(self.pos[0], self.pos[1], 32, 32)
-
-    @property
+        self.speed = 5
+    @property #Ca sert à quoi ?
     def x(self):
         return self.pos[0]
-    @property
+    @property #Ca aussi ?
     def y(self, x):
         self.pos[1] = x
 
     def tp(self, x, y):
         self.pos = [x, y]
+
+    def move(self, dx, dy):
+        self.pos[0]+= dx*self.speed
+        self.pos[1]+= dy*self.speed
+        self.rect.x, self.rect.y = self.pos[0], self.pos[1]
+    def Draw(self,surface):
+        pygame.draw.rect(surface,(255,0,0),self.rect)
