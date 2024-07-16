fb60ee3768293f11abde22266ca9f601ff49f9fa
WannouII <mathysprudhommealbert@gmail.com>
2024-07-16 16:36:17 +0200

    Update worldgen.py

--- src/worldgen.py
+++ src/worldgen.py
@@ -4,7 +4,12 @@ import perlinNoise
 import matplotlib.pyplot as plt
 
 noise=perlinNoise.Perlin()
-x=[i*0.01 for i in range(100)]
-y= [noise.valueAt(i) for i in x]
+x =[i*0.01 for i in range(1000)]
+y = [0 for i in range(1000)]
+for i in range(10):
+    j = [(noise.valueAt(k))/2**(2*i) *(-1)**i for k in x]
+    for h in range(len(y)):
+        y[h]+=(j[h])
+
 plt.plot(x,y)
 plt.show()
